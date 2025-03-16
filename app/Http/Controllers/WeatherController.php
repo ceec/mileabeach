a36fae400c15b65eecc42b122791b7c99bdb105a
Christine <cc@battab.com>
2025-03-15 21:17:23 -0600

    Update WeatherController.php
    what am I returning

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -18,7 +18,7 @@ class WeatherController extends Controller
         //$weather = Weather::orderBy('created_at','desc')->first();
         $weather = Weather::latest('id')->limit(1)->get();
 
-
+        dd($weather);
         
         return  view('home')
         ->with('weather',$weather);
